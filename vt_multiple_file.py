c5d9943a182e628b366909ffa573b81cf136da96
yadhukm <yadhukrishnanm44@gmail.com>
2021-05-29 07:59:26 +0530

    Create vt_multiple_file.py

---
+++ vt_multiple_file.py
@@ -0,0 +1,13 @@
+mport requests
+import json
+
+API_KEY= '93174196079fc424e2d5676fea92b601c7a9e68d76feddb88d9bf4c509a065cd'
+
+check = 'google.com'
+
+url = 'https://www.virustotal.com/vtapi/v2/url/report'
+
+params = {'apikey': API_KEY, 'resource': check}
+
+response = requests.get(url, params=params)
+
