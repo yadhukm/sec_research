56cad37ebab66c69b179bfa374621bba5121fe28
yadhukm <yadhukrishnanm44@gmail.com>
2021-05-30 03:06:05 -0400

    Update vt.py

--- vt.py
+++ vt.py
@@ -3,7 +3,7 @@ import json
 
 API_KEY= '93174196079fc424e2d5676fea92b601c7a9e68d76feddb88d9bf4c509a065cd'
 
-check = 'google.com'
+check = input("enter the url in double quotes: ")
 
 url = 'https://www.virustotal.com/vtapi/v2/url/report'
 
@@ -11,5 +11,21 @@ params = {'apikey': API_KEY, 'resource': check}
 
 response = requests.get(url, params=params)
 
-print(response.json())
+response_json = json.loads(response.content)
+
+y= response_json['positives']
+
+x=response_json['url']
+
+print(x)
+
+if y==0:
+        print("non malicious")
+elif y>0 and y<50:
+        print("suspicious")
+elif y>50:
+        print ("higly suspicious")
+
+else:
+        print("url not_detected")
 
